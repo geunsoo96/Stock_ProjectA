41e922ef4d2bdb49525a1ddd8e11a1be56f3efe8
jeonhyoungmin <jhmin7509@gmail.com>
2022-11-08 14:59:36 +0900

    뉴스 라우트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import Main from './Layout/Main';
 import {Routes,Route} from 'react-router-dom';
 import Top from './Pages/Top/Top';
+import News from './Pages/News/News';
+import './App.css'
 
 const App = () => {
 
     return(<>
     <Routes>
     <Route path='/' element={<Main/>}>
-    <Route path='/' element={<Top/>}></Route>
-    </Route>
+    <Route path='/Top' element={<Top/>}></Route>
+    <Route path='/News' element={<News/>}></Route>
+    </Route> 
     </Routes>
     </>)
 }
