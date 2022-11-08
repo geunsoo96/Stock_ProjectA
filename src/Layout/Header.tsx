a2bc856bdb56fcbf2327111a4cb52a8338e6cae4
KimGeunSoo <geunsoo1106@gmail.com>
2022-11-08 14:32:05 +0900

    폰트 세팅

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,59 +1,58 @@
 import styled from "styled-components";
 
 const HeaderBox = styled.div`
-width:1920px;
-height:150px;
-display: flex;
-align-items: center;
-gap: 200px;
-flex-direction: row;
-background-color: rgba(254, 172, 49, 1);
-color:white;
+  width: 1920px;
+  height: 150px;
+  display: flex;
+  align-items: center;
+  gap: 200px;
+  flex-direction: row;
+  background-color: rgba(254, 172, 49, 1);
+  color: white;
 
-&>div{
+  & > div {
+    font-family: "SCD-3";
     font-size: 2rem;
     font-weight: bold;
-    width:200px;
-    height:80px;
+    width: 200px;
+    height: 80px;
     display: flex;
     align-items: center;
     justify-content: center;
     background-color: gray;
     border-radius: 20px;
-    &:hover{
-        color:black;
-        cursor: pointer;
+    &:hover {
+      color: black;
+      cursor: pointer;
     }
-}
+  }
 
-&>input{
+  & > input {
     width: 400px;
-    height:80px;
+    height: 80px;
     margin-left: 150px;
     font-size: 2rem;
     font-weight: bold;
     border-radius: 10px;
     padding-left: 20px;
-}
-&>img{
+  }
+  & > img {
     cursor: pointer;
     margin-left: 100px;
-    
-}
-`
+  }
+`;
 
 const Header = () => {
-
-    return(<>
-    <HeaderBox>
-    <input placeholder="통합검색"></input>
-    <div>추천 알고리즘</div>
-    <div>최근 주식 동향</div>
-    <img src="/img/logo.png" alt="이미지 없음"/>
-    </HeaderBox>
-    </>)
-
-
-}
+  return (
+    <>
+      <HeaderBox>
+        <input placeholder="통합검색"></input>
+        <div>추천 알고리즘</div>
+        <div>최근 주식 동향</div>
+        <img src="/img/logo.png" alt="이미지 없음" />
+      </HeaderBox>
+    </>
+  );
+};
 
 export default Header;
