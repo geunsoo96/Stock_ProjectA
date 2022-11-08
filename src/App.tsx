af3d283c855be432b1b57168a35b2260b6414ed9
KimGeunSoo <geunsoo1106@gmail.com>
2022-11-08 15:03:52 +0900

    충돌 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import Main from './Layout/Main';
-import {Routes,Route} from 'react-router-dom';
-import Top from './Pages/Top/Top';
+import Main from "./Layout/Main";
+import { Routes, Route } from "react-router-dom";
+import Top from "./Pages/Top/Top";
 import "./App.css";
 
 const App = () => {
-
-    return(<>
-    <Routes>
-    <Route path='/' element={<Main/>}>
-    <Route path='/' element={<Top/>}></Route>
-    </Route>
-    </Routes>
-    </>)
-}
-
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route path="/" element={<Top />}></Route>
+        </Route>
+      </Routes>
+    </>
+  );
+};
+//
 export default App;
