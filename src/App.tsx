2d45271684188c5f0a744d67a3d7e5d90ec04279
KimGeunSoo <geunsoo1106@gmail.com>
2022-11-08 15:22:24 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Top from "./Pages/Top/Top";
 import News from "./Pages/News/News";
 import "./App.css";
+import Algorithm from "./Pages/Algorithm/Algorithm";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Main />}>
           <Route path="/Top" element={<Top />}></Route>
           <Route path="/News" element={<News />}></Route>
+          <Route path="/Algorithm" element={<Algorithm />}></Route>
         </Route>
       </Routes>
     </>
---
+++ src/Pages/Algorithm/Algorithm.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+const Algorithm = () => {
+  return <></>;
+};
+export default Algorithm;
