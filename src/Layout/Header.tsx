5f689f65b411bc9084f755f3e4a85b35f93f268a
Yehoonn <jes6208@naver.com>
2022-11-08 12:46:38 +0900

    'main-update'

---
+++ src/Layout/Header.tsx
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+const HeaderBox = styled.div`
+width:1920px;
+height:150px;
+display: flex;
+align-items: center;
+gap: 200px;
+flex-direction: row;
+background-color: rgba(254, 172, 49, 1);
+color:white;
+
+&>div{
+    font-size: 2rem;
+    font-weight: bold;
+    width:200px;
+    height:80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: gray;
+    border-radius: 20px;
+    &:hover{
+        color:black;
+        cursor: pointer;
+    }
+}
+
+&>input{
+    width: 400px;
+    height:80px;
+    margin-left: 150px;
+    font-size: 2rem;
+    font-weight: bold;
+    border-radius: 10px;
+    padding-left: 20px;
+}
+&>img{
+    cursor: pointer;
+    margin-left: 100px;
+    
+}
+`
+
+const Header = () => {
+
+    return(<>
+    <HeaderBox>
+    <input placeholder="통합검색"></input>
+    <div>추천 알고리즘</div>
+    <div>최근 주식 동향</div>
+    <img src="/img/logo.png" alt="이미지 없음"/>
+    </HeaderBox>
+    </>)
+
+
+}
+
+export default Header;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+import './index.css';
 
-const rootElement = document.getElementById("root");
+
+const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement as Element);
 
 root.render(
-  <>
+    <BrowserRouter>
     <App />
-  </>
+    </BrowserRouter>
 );
