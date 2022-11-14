942434db1382cc965ea216c9cab0cae30368a744
KinGeunSoo <geunsoo1106@gmail.com>
2022-11-14 19:16:23 +0900

    flask 설치

---
+++ BackEnd/myvenv/Scripts/app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from dbconn import data_by_code , search_company_name , company_name
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+app.config['JSON_AS_ASCII'] = False
+
+@app.route('/')
+def home():
+  return 'hello world'
+
+@app.route('/code/<code>')
+def code(code):
+  data = data_by_code(code)
+  return data
+  
+@app.route('/name/<name>')
+def name(name):
+  data = search_company_name(name)
+  return data
+
+@app.route('/randomName')
+def randomName():
+  data = company_name()
+  return data
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
