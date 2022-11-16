524a45a8bb5bfd19d1cb879d965395ff31bc10b5
jongin12 <jongin981224@gmail.com>
2022-11-16 17:53:34 +0900

    'dd'

--- BackEnd/myvenv/Scripts/dbconn.py
+++ BackEnd/myvenv/Scripts/dbconn.py
@@ -89,3 +89,49 @@ def company_name_byCode(code):
   results = cur.fetchone()
   conn.close()
   return results
+
+def all_company_rank(market,day,column):
+  conn = dbconnect()
+  cur = conn.cursor()
+  sql = f'SELECT TABLE_NAME FROM information_schema.tables WHERE TABLE_NAME LIKE "{market}%" and TABLE_NAME LIKE "%_{day}"'
+  cur.execute(sql)
+  company = cur.fetchall()
+  arr = []
+  for name in company:
+    sql = f'SELECT {column} FROM {name["TABLE_NAME"]} ORDER BY day DESC limit 1'
+    cur.execute(sql)
+    data = cur.fetchone()
+    if data != None:
+      arr.append(data[column])
+    else:
+      arr.append(0)
+  sort = sorted(arr)
+  sort.sort(reverse=True)
+  rank = sort[0:10]
+  answer = []
+  name = []
+  close = []
+  for i in range(len(rank)):
+    index = arr.index(rank[i])
+    obj = {
+      "DB":company[index]["TABLE_NAME"],
+      "value":arr[index],
+    }
+    answer.append(obj)
+  
+  for item in answer:
+    name = item['DB']
+    find = name.split('_')
+    sql = f'SELECT name FROM `stock586`.`companyList` WHERE code = {find[1]}'
+    cur.execute(sql)
+    data = cur.fetchone()
+    item["name"] = data["name"]
+    sql = f'SELECT close FROM {name} ORDER BY day DESC limit 1'
+    cur.execute(sql)
+    data2 = cur.fetchone()
+    item["close"] = data2["close"]
+  
+  conn.close()
+  return answer
+
+# all_company_rank('kospi','m','volume')
